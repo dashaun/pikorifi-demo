e4e2b315444aa4deb9a4981b2b4d23badfa5c942
dashaun <dashaun@dashaun.com>
2025-01-13 21:57:29 -0600

    demo.pikorifi.cc

---
+++ src/main/java/com/example/korifidemo/KorifidemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.korifidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorifidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KorifidemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/korifidemo/KorifidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.korifidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorifidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
